cf6fc9f0b87522d0bef67d234b325b0e0b4135ad
Piotr Boruc <wasilp01@stepstone.com>
2019-01-13 21:51:22 +0100

    improved logging

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 import socket
-
+import logging
+import sys
 from flask import Flask, jsonify, request, session
 
 app = Flask(__name__)
 app.secret_key = "cne287fg8237hc38igochh98cy^TR^&%R&T*&G"
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 
 
 @app.route('/health')
@@ -28,9 +32,13 @@ def hello_world():
     session["hostnames"][hostname] += 1
     session["counter"] += 1
 
-    return jsonify({
+    debug_msg = {
         "counter": session["counter"],
         "host": socket.getfqdn(),
         "hostnames": session["hostnames"],
         "request": str(request),
-    })
+    }
+
+    app.logger.debug(debug_msg)
+
+    return jsonify(debug_msg)
